84b583e610b71b7187fe9c8e8fbbc3e6f91e1e3b
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2024-03-28 17:05:16 +0100

    feat(web): update index.md

--- apps/web/components/content/Heading.vue
+++ apps/web/components/content/Heading.vue
@@ -5,7 +5,7 @@ const { locale } = useI18n()
 </script>
 
 <template>
-  <h1 class="text-lg sm:text-xl col-span-1 sm:col-span-2 font-bold p-4 sm:p-6 my-1 sm:my-2 text-center uppercase bg-gray-200 text-gray-900 dark:bg-gray-800 dark:text-white not-prose">
+  <h1 class="text-lg sm:text-xl rounded-md col-span-1 shadow-inner sm:col-span-2 font-bold p-4 sm:p-6 my-1 sm:my-2 text-center uppercase bg-gray-200 text-gray-900 dark:bg-gray-800 dark:text-white not-prose">
     <slot :name="locale" />
   </h1>
 </template>
